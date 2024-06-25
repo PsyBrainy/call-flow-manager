a89ed11269c25d9e7cd109844675d5ae32c4baf3
martu <martuto93@gmail.com>
2024-06-25 18:43:57 -0300

    initial commit

---
+++ src/main/java/com/psybrainy/CallFlowManager/CallFlowManagerApplication.java
@@ -0,0 +1,13 @@
+package com.psybrainy.CallFlowManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallFlowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallFlowManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/psybrainy/CallFlowManager/CallFlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psybrainy.CallFlowManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallFlowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
